f6dd4486d1567a0a43acfef3833dfb87ea1de36b
Allen Hai <allenhai03@gmail.com>
2018-11-09 11:53:46 -0600

    add console.log to counter hook to observe re-render behavior

--- components/Counter.js
+++ components/Counter.js
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+import {useState, useEffect} from 'react'
 
-export default function Counter () {
+export default function Counter() {
   const [count, setCount] = useState(0)
-  return <div>
-    <p>{count}</p>
-    <button onClick={() => setCount(count + 1)}>
-      inc
-    </button>
-    <button onClick={() => setCount(count - 1)}>
-      dec
-    </button>
-  </div>
+
+  useEffect(() => console.log('re-rendered'))
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>inc</button>
+      <button onClick={() => setCount(count - 1)}>dec</button>
+    </div>
+  )
 }
